2eff50dbacc129c73b977b7359f2f59d78a9a2c1
djbragas <windows888xxx@gmail.com>
2018-06-25 23:09:47 +0300

    Thread safe(XUnit runs test concurently) MapperInitializer was added.

---
+++ KatlaSport.Services.Tests/Infrastructure/MapperInitializer.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using KatlaSport.Services.HiveManagement;
+
+namespace KatlaSport.Services.Tests
+{
+    internal class MapperInitializer
+    {
+        private static bool _initizlized = false;
+        private static object _lock = new object();
+
+        public static void Initialize()
+        {
+            lock (_lock)
+            {
+                if (!_initizlized)
+                {
+                    Mapper.Initialize(cfg => cfg.AddProfile(new HiveManagementMappingProfile()));
+                    _initizlized = true;
+                }
+            }
+        }
+    }
+}
