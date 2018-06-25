4cf4ed5035ad529c1faa2417c23a25473de0a2a8
djbragas <windows888xxx@gmail.com>
2018-06-25 19:19:17 +0300

    Hive management. Tests for HiveService was implemented.

--- KatlaSport.Services.Tests/HiveManagement/HiveServiceTests.cs
+++ KatlaSport.Services.Tests/HiveManagement/HiveServiceTests.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using KatlaSport.DataAccess.ProductStoreHive;
+using KatlaSport.Services.HiveManagement;
+using Moq;
+using Xunit;
 
 namespace KatlaSport.Services.Tests.HiveManagement
 {
     public class HiveServiceTests
     {
+        [Fact]
+        public void Ctor_UserContextIsNull_ArgumentNullExceptionThrown()
+        {
+            var userContext = new Mock<IUserContext>();
+            var exception = Assert.Throws<ArgumentNullException>(() => new HiveService(null, userContext.Object));
+
+            Assert.Equal(typeof(ArgumentNullException), exception.GetType());
+        }
+
+        [Fact]
+        public void Ctor_ContextIsNull_ArgumentNullExceptionThrown()
+        {
+            var context = new Mock<IProductStoreHiveContext>();
+            var exception = Assert.Throws<ArgumentNullException>(() => new HiveService(context.Object, null));
+
+            Assert.Equal(typeof(ArgumentNullException), exception.GetType());
+        }
     }
 }
